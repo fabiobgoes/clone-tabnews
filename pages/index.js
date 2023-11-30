394c6244d3b462cdba8b5df826a32704be513ac4
fabiobgoes <fabiob.goes@gmail.com>
2023-11-30 14:53:52 +0000

    alteraçao da frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Ida, a sua vida é o maior empreendimento da sua vida. Dela dependem todos os outros empreendimentos. Amo você. Se você me ama, dá uma risadinha.</h1>
+    return <h1>Ida, a sua vida é o maior empreendimento da sua vida, não deixe ela ir à falência. Dela dependem todos os outros empreendimentos. Amo você. Se você me ama, dá uma risadinha.</h1>
 }
 
 export default Home;
