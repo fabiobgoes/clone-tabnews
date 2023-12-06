5b8f97bed7c9331db1e7b8a9e560ba006fea751a
fabiobgoes <fabiob.goes@gmail.com>
2023-12-06 14:17:32 +0000

    adiciona scripts  e

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
-function Home(){
-    return <h1>Ida, a sua vida é o maior empreendimento da sua vida, não deixe ela ir à falência. Dela dependem todos os outros empreendimentos. Amo você. Se você me ama, dá uma risadinha.</h1>
+function Home() {
+  return (
+    <h1>
+      Ida, a sua vida é o maior empreendimento da sua vida, não deixe ela ir à
+      falência. Dela dependem todos os outros empreendimentos. Amo você. Se você
+      me ama, dá uma risadinha.
+    </h1>
+  );
 }
 
 export default Home;
