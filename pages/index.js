efcd62f063941fed51855d0ed4d3a5c3a38f1200
Fabio <fabiob.goes@gmail.com>
2024-03-29 12:26:48 -0400

    retomando o curso

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,7 @@ function Home() {
   return (
     <h1>
       Ida, a sua vida é o maior empreendimento da sua vida, não deixe ela ir à
-      falência. Dela dependem todos os outros empreendimentos. Amo você. Se você
-      me ama, dá uma risadinha.
+      falência. Dela dependem todos os outros empreendimentos. Amo vocês.
     </h1>
   );
 }
