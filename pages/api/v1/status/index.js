cb4e383fd03afa61273f4bc60f923be0b48e0a30
Fabio <fabiob.goes@gmail.com>
2024-04-04 19:59:00 -0400

    add  and

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,11 @@
-function status(request, response) {
-  response.status(200).json(
-    "alunos do curso.dev são pessoas acima " +
-    "da média_.");
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 AS SUM;");
+  console.log(result.rows);
+  response
+    .status(200)
+    .json("alunos do curso.dev são pessoas acima " + "da média_.");
 }
 
 export default status;
